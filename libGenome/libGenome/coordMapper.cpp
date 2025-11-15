421312a8d4f9c16aa32c7ece75f28e6b88c19d22
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-14 22:00:44 -0500

    Update coordMapper.cpp

--- libGenome/libGenome/coordMapper.cpp
+++ libGenome/libGenome/coordMapper.cpp
@@ -6,6 +6,16 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <list>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::vector;
+using std::list;
 
 struct ExMem{
     gnSeqI length;
@@ -22,28 +32,28 @@ static boolean LocationEndLessthan(const gnLocation& a, const gnLocation& b){
 }
 
 static boolean LocationSizeLessthan(const gnLocation& a, const gnLocation& b){
-    return a.GetEnd() - a.GetStart() < b.GetEnd() - b.GetStart();
+    return (a.GetEnd() - a.GetStart()) < (b.GetEnd() - b.GetStart());
 }
 
 static boolean ExMemLessthan(const ExMem& a, const ExMem& b){
     if(a.ex_start == b.ex_start){
-        if(a.in_start == b.in_start){
+        if(a.in_start == b.in_start)
             return a.length < b.length;
-        }
         return a.in_start < b.in_start;
     }
     return a.ex_start < b.ex_start;
 }
 
 void print_usage(char* pname){
-    cout << "Usage: " << pname << " <genbank file> <exon_list> <intron_list> "
-         << "<mem_list> <regulation_network> <minimum match size>\n";
+    cout << "Usage: " << pname
+        << " <genbank file> <exon_list> <intron_list> <mem_list>"
+        << " <regulation_network> <minimum match size>\n";
 }
 
 void load_map_file(string& filename, vector<gnLocation>& loc_list){
     ifstream coord_file(filename.c_str());
     if(!coord_file.is_open()){
-        cout << "couldn't open file\n";
+        cout << "couldn't open file: " << filename << "\n";
         return;
     }
 
@@ -52,18 +62,17 @@ void load_map_file(string& filename, vector<gnLocation>& loc_list){
         coord_file >> start_coord;
         coord_file >> end_coord;
 
-        gnLocation new_location(start_coord, end_coord);
-        loc_list.push_back(new_location);
+        if(coord_file.good()){
+            loc_list.push_back(gnLocation(start_coord, end_coord));
+        }
     }
 }
 
 void map_coordinates(vector<gnLocation>& loc_list, vector<gnLocation>& map_list){
     gnSeqI curStart = 1;
-    for(uint32 locationI = 0; locationI < loc_list.size(); locationI++){
-        gnSeqI cur_len = loc_list[locationI].GetEnd() -
-                         loc_list[locationI].GetStart() + 1;
-        gnLocation map_location(curStart, curStart + cur_len - 1);
-        map_list.push_back(map_location);
+    for(uint32 i = 0; i < loc_list.size(); i++){
+        gnSeqI cur_len = loc_list[i].GetEnd() - loc_list[i].GetStart() + 1;
+        map_list.push_back(gnLocation(curStart, curStart + cur_len - 1));
         curStart += cur_len;
     }
 }
@@ -73,15 +82,15 @@ void map_list_coordinates(list<gnLocation>& loc_list, list<gnLocation>& map_list
     for(list<gnLocation>::iterator it = loc_list.begin();
         it != loc_list.end(); ++it){
         gnSeqI cur_len = it->GetEnd() - it->GetStart() + 1;
-        gnLocation map_location(curStart, curStart + cur_len - 1);
-        map_list.push_back(map_location);
+        map_list.push_back(gnLocation(curStart, curStart + cur_len - 1));
         curStart += cur_len;
     }
 }
 
 uint32 loc_binary_search(vector<gnLocation>& loc_list,
                          uint32 startI, uint32 endI,
-                         gnLocation& query_loc){
+                         gnLocation& query_loc)
+{
     uint32 midI = ((endI - startI) / 2) + startI;
     if(startI == endI)
         return endI;
@@ -140,17 +149,16 @@ int main(int argc, char* argv[]){
     load_map_file(exon_list_filename, exon_list);
     load_map_file(intron_list_filename, intron_list);
 
-    cout << exon_list.size() << " unique exons loaded from file\n";
-    cout << intron_list.size() << " unique introns loaded from file\n";
+    cout << exon_list.size() << " unique exons loaded\n";
+    cout << intron_list.size() << " unique introns loaded\n";
 
     gnSequence seq_file;
     if(!seq_file.LoadSource(seq_filename)){
-        cout << "Error loading file\n";
+        cout << "Error loading genbank file\n";
         return -1;
     }
-    cout << "Sequence loaded successfully, "
-         << seq_file.length()
-         << " base pairs.\n";
+
+    cout << "Sequence loaded, " << seq_file.length() << " bp\n";
 
     map_coordinates(exon_list, exon_map_list);
     map_coordinates(intron_list, intron_map_list);
@@ -161,32 +169,37 @@ int main(int argc, char* argv[]){
         mem_file >> m.ex_start;
         mem_file >> m.in_start;
 
-        if(m.length >= minimum_match_size)
+        if(mem_file.good() && m.length >= minimum_match_size)
             mem_list.push_back(m);
     }
 
-    cout << mem_list.size() << " matches loaded.\n";
+    cout << mem_list.size() << " MEMs loaded\n";
+
     sort(mem_list.begin(), mem_list.end(), &ExMemLessthan);
 
     uint32 exonmapI = 0;
     uint32 notify_percent = 10;
     uint32 notify_interval =
-        (notify_percent > 0 && mem_list.size() > notify_percent)
-        ? mem_list.size() / notify_percent
-        : 1;
+        (mem_list.size() > notify_percent ? mem_list.size() / notify_percent : 1);
     uint32 percent_count = 0;
 
     cout << "Searching for complementary matches:\n";
 
+    // Reintroduced variables (previously inside removed block)
+    gnLocation ex_map_loc(0, 0);
+    gnLocation in_map_loc(0, 0);
+
     for(uint32 memI = 0; memI < mem_list.size(); memI++){
+
         if(memI % notify_interval == 0){
             cout << percent_count << "%.. ";
             percent_count += notify_percent;
         }
 
-        gnLocation ex_map_loc(mem_list[memI].ex_start,
-                              mem_list[memI].ex_start +
-                              mem_list[memI].length - 1);
+        // Compute exon mapping location
+        ex_map_loc = gnLocation(mem_list[memI].ex_start,
+                                mem_list[memI].ex_start +
+                                mem_list[memI].length - 1);
 
         for(; exonmapI < exon_map_list.size(); exonmapI++){
             if(exon_map_list[exonmapI].Intersects(ex_map_loc))
@@ -200,19 +213,18 @@ int main(int argc, char* argv[]){
         }
         mapEnd--;
 
-        uint32 intronmapI = 0;
         int64 cur_in_start = mem_list[memI].in_start;
         if(cur_in_start < 0)
             cur_in_start = -cur_in_start;
 
-        gnLocation in_map_loc(cur_in_start,
-                              cur_in_start + mem_list[memI].length - 1);
+        in_map_loc = gnLocation(cur_in_start,
+                                cur_in_start + mem_list[memI].length - 1);
 
-        uint32 search_mapI = loc_binary_search(intron_map_list,
-                                               0,
-                                               intron_map_list.size()-1,
-                                               in_map_loc);
-        intronmapI = search_mapI;
+        uint32 search_mapI =
+            loc_binary_search(intron_map_list, 0,
+                              intron_map_list.size() - 1, in_map_loc);
+
+        uint32 intronmapI = search_mapI;
 
         for(; intronmapI >= 0; intronmapI--){
             if(!intron_map_list[intronmapI].Intersects(in_map_loc)){
@@ -230,4 +242,5 @@ int main(int argc, char* argv[]){
         }
         intron_mapEnd--;
     }
+    return 0;
 }
