1d9628108c845ee297d7f1ab0d407c2cc1d04dbf
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-16 00:24:53 -0500

    Update gnAlignedSequences.cpp

--- libMems/libMems/gnAlignedSequences.cpp
+++ libMems/libMems/gnAlignedSequences.cpp
@@ -1551,7 +1551,7 @@ bool gnAlignedSequences::coordinates(string line)
 {
 	bool toReturn = true;
 	
-	for (int i=0; i<line.length(); i++)
+	for (size_t i=0; i<line.length(); i++)
 	{
 		if (line[i]!=' ' && line[i]!='\r' && line[i]!='\n' && (line[i]<48 || line[i]>57))
 		{
