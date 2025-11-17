1e355e5f3affc4a28df65b861606a2da1919864e
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-16 20:42:09 -0500

    Update gnProteinSequence.h

--- libGenome/libGenome/gnProteinSequence.h
+++ libGenome/libGenome/gnProteinSequence.h
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////
-// File:            gnRAWSequence.h
-// Purpose:         Optimized Sequence class for RAW sequence files
+// File:            gnProteinSequence.h
+// Purpose:         Sequence class
 // Description:     Provides a high level sequence interface to all types of
 //					sequence data.
 // Changes:        
@@ -10,194 +10,120 @@
 // Copyright:       (c) Aaron Darling 
 // Licenses:        See COPYING file for details 
 /////////////////////////////////////////////////////////////////////////////
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
-#ifndef _gnRAWSequence_h_
-#define _gnRAWSequence_h_
+#ifndef _gnProteinSequence_h_
+#define _gnProteinSequence_h_
 
 #include "libGenome/gnDefs.h"
 
 #include <string>
-#include <iostream>
 #include <list>
 #include "libGenome/gnSequence.h"
+#include "libGenome/gnFilter.h"
+
 
 namespace genome {
 
-
 /**
- * gnRAWSequence is a bastardization of gnSequence that creates a lightweight wrapper
- * around a memory-mapped file of raw sequence data
+ * gnProteinSequence is a special kind of gnSequence which can be used for RNA sequences
+ * It sets the default filters and comparators to the RNA filters and comparators.
  */
-class GNDLLEXPORT gnRAWSequence : public gnSequence
+
+class GNDLLEXPORT gnProteinSequence : public gnSequence
 {
 public:
 	/**
-	 * Empty Constructor, creates an empty gnRAWSequence.
+	 * Empty Constructor, creates an empty gnProteinSequence.
 	 */
-	gnRAWSequence();
+	gnProteinSequence();
 	/**
-	 * Creates a gnRAWSequence based on the file specified by filename
+	 * Creates a gnProteinSequence with a single contig containing the bases in "seq".
+	 * @param seq The null terminated array of base pairs to use.
 	 */
-	gnRAWSequence( const std::string& filename )
-	{
-		this->filename = filename;
-		data.open( filename );
-	}
-
-	gnRAWSequence* Clone() const {return new gnRAWSequence(*this);}
-
-	gnSeqI contigListSize() const {return 1;}
-	gnSeqI contigListLength() const {return 1;}
-	uint32 contigIndexByBase( const gnSeqI baseI) const {
-		if(baseI >= data.size()) Throw_gnEx(SeqIndexOutOfBounds());
-		return 0;
-	}
-	gnRAWSequence contig( const uint32 contigI) const { 
-		if(contigI>0) Throw_gnEx(FragmentIndexOutOfBounds()); 
-		return *this;
-	}
-	gnRAWSequence contigByBase( const gnSeqI baseI) const {
-		if(baseI >= data.size()) Throw_gnEx(SeqIndexOutOfBounds());
-		return *this;
-	}
-	virtual gnSeqI contigStart( const uint32 contigI) const {
-		if(contigI>0) Throw_gnEx(FragmentIndexOutOfBounds()); 
-		return 0;
-	}
-	virtual gnSeqI contigLength( const uint32 contigI) const {
-		if(contigI>0) Throw_gnEx(FragmentIndexOutOfBounds()); 
-		return data.size();
-	}
-	virtual uint32 contigIndexByName( std::string& contigName) const {
-		return 0;
-	}
-	virtual std::string contigName( const uint32 contigI) const {
-		if(contigI>0) Throw_gnEx(FragmentIndexOutOfBounds()); 
-		return "";
-	}
-	virtual gnSequence contigByName( std::string& contigName) const {
-		Throw_gnEx(FragmentIndexOutOfBounds());
-	}
-	virtual void merge([[maybe_unused]] const gnSeqI startI, [[maybe_unused]] const gnSeqI endI){ throw; }
-	virtual void mergeContigs([[maybe_unused]] const uint32 startC, [[maybe_unused]] const uint32 endC){ throw; }
-	virtual void splitContig([[maybe_unused]] const gnSeqI splitI, [[maybe_unused]] const uint32 contigI=ALL_CONTIGS) { throw; }
-
-	virtual void setContigName( [[maybe_unused]] const uint32 contigI, [[maybe_unused]] const std::string& contig_name) { throw; }
-
-	virtual uint32 getFeatureListLength() const {
-		return 0;
-	}
-	virtual gnBaseFeature* getFeature([[maybe_unused]] const uint32 featureI) const{ Throw_gnEx(FeatureIndexOutOfBounds()); }
-	virtual void getContainedFeatures([[maybe_unused]] const gnLocation& lt, [[maybe_unused]] std::vector<gnBaseFeature*>& feature_vector, [[maybe_unused]] std::vector<uint32>& index_vector) const {}
-	virtual void getIntersectingFeatures([[maybe_unused]] const gnLocation& lt, [[maybe_unused]] std::vector<gnBaseFeature*>& feature_vector, [[maybe_unused]] std::vector<uint32>& index_vector) const {}
-	virtual uint32 addFeature([[maybe_unused]] gnBaseFeature* feature) { throw; }
-	virtual void removeFeature([[maybe_unused]] const uint32 featureI){ Throw_gnEx(FeatureIndexOutOfBounds()); }
-	virtual void getBrokenFeatures([[maybe_unused]] const gnLocation& lt, [[maybe_unused]] std::vector<gnBaseFeature*>& feature_vector) const{};
-	virtual uint32 getHeaderListLength([[maybe_unused]] const uint32 contigI) const{ return 0; }
-	virtual gnBaseHeader* getHeader([[maybe_unused]] const uint32 contigI, [[maybe_unused]] const uint32 headerI) const{Throw_gnEx(HeaderIndexOutOfBounds());};
-	virtual void addHeader([[maybe_unused]] const uint32 contigI, [[maybe_unused]] gnBaseHeader* header, [[maybe_unused]] const uint32 headerI){Throw_gnEx(FragmentIndexOutOfBounds());}
-	virtual void removeHeader([[maybe_unused]] const uint32 contigI, [[maybe_unused]] const uint32 headerI){ Throw_gnEx(HeaderIndexOutOfBounds()); }
-	virtual void setReverseComplement( const bool revComp, const uint32 contigI=ALL_CONTIGS){Throw_gnEx(FragmentIndexOutOfBounds());};
-	virtual bool isReverseComplement( const uint32 contigI=ALL_CONTIGS ){return false;}
-	virtual bool isCircular() const{ return false; }
-	virtual void setCircular( const bool value ) {}
-	
-	virtual void globalToLocal(uint32& contigI, gnSeqI& baseI) const{};
-	virtual void localToGlobal(const uint32 contigI, gnSeqI& baseI) const {};
-	virtual void globalToSource(uint32& contigI, gnSeqI& baseI) const{};
-	virtual void localToSource(uint32& contigI, [[maybe_unused]] gnSeqI& baseI) const{};
-	virtual bool LoadSource(const std::string sourcename){
-		data.open( sourcename );
-		filename = sourcename;
-		return true;
-	}
-
+	gnProteinSequence( const gnSeqC* seq );
 	/**
-	 * Appends the bases in "seq" to this sequence.
+	 * Creates a gnProteinSequence with a single contig containing the bases in "str".
+	 * @param str The base pairs to use.
 	 */
-	gnRAWSequence& operator+=(const gnRAWSequence& seq);
-
+	gnProteinSequence( const std::string& str );
 	/**
-	 * Compares the bases in "seq" to this sequence.
-	 * @param seq The sequence to compare this sequence to.
-	 * @return Negative if this sequence is lesser, 0 if the two sequences are
-	 * equal, and positive if this sequence is greater.
+	 * Creates a gnProteinSequence with the contigs stored in "gngs".
+	 * @param gngs the gnGenomeSpec to get contigs from.
 	 */
-/*	virtual int compare(const gnRAWSequence& seq) const;
-	virtual int compare(const std::string& str) const;
-
-	virtual void append( const gnRAWSequence& seq);
-	virtual void insert( const gnSeqI offset, const gnSeqC *bases, const gnSeqI length);
-	virtual void insert( const gnSeqI offset, const gnRAWSequence& seq);
-	virtual void insert( const gnSeqI offset, const gnGenomeSpec& gnbs);
-	gnRAWSequence const operator+(const gnRAWSequence& seq) const;
-	virtual void erase( const gnSeqI offset=0, const gnSeqI length=GNSEQI_END );
-*/
-	gnRAWSequence subseq(const gnSeqI offset, const gnSeqI length) const
-	{
-		gnRAWSequence gnrs;
-		gnrs.data.open(filename, length, offset - 1);
-		return gnrs;
-	}
-//	friend std::istream& operator>>(std::istream& is, gnRAWSequence& gns);	//read from source.
+	gnProteinSequence( const gnGenomeSpec& gngs );
 	/**
-	 * Writes the bases in this sequence to the specified output stream (e.g. cout).
+	 * Creates a gnProteinSequence with the contigs stored in "gnfs".
+	 * @param gnfs the gnFragmentSpec to get contigs from.
 	 */
-//	friend std::ostream& operator<<(std::ostream& os, const gnRAWSequence& gns); //write to source.
-
-	virtual gnSeqI length() const { return data.size(); }
-	virtual gnSeqI size() const { return data.size(); }
-
-	virtual std::string ToString( const gnSeqI length=GNSEQI_END, const gnSeqI offset=1 ) const
-	{
-		gnSeqI len = length == GNSEQI_END ?  data.size() - offset - 1 : length;
-		std::string asdf(data.data()+offset-1, len);
-		return asdf;
-	}
-
-	virtual bool ToString( std::string& str, const gnSeqI length=GNSEQI_END, const gnSeqI offset=1 ) const
-	{
-		gnSeqI len = length == GNSEQI_END ? data.size() - offset - 1 : length;
-		str.assign(data.data()+offset-1,len);
-		return true;
-	}
-	virtual bool ToArray( gnSeqC* pSeqC, gnSeqI length, const gnSeqI offset=1 ) const
-	{
-		gnSeqI len = length == GNSEQI_END ? data.size() - offset - 1 : length;
-		memcpy(pSeqC, data.data()+offset-1, len);
-		return true;
-	}
-	virtual gnSeqC GetSeqC( const gnSeqI offset ) const
-	{
-		return *(data.data()+(offset-1));
-	}
-	gnSeqC operator[]( const gnSeqI offset ) const
-	{
-		return *(data.data()+(offset-1));
-	}
-
-	virtual gnSeqI find([[maybe_unused]] const gnSequence& search, [[maybe_unused]] const gnSeqI offset=0) const override {return GNSEQI_ERROR;}
-	virtual gnSeqI find([[maybe_unused]] const gnRAWSequence& search, [[maybe_unused]] const gnSeqI offset=0) const {return GNSEQI_ERROR;}
-	
+	gnProteinSequence( const gnFragmentSpec& gnfs );
+	/**
+	 * Creates a gnProteinSequence with the contigs stored in "gncs".
+	 * @param gncs the gnContigSpec to get contigs from.
+	 */
+	gnProteinSequence( const gnContigSpec& gncs );
+	/**
+	 * Creates a gnProteinSequence with a single contig containing the bases in "bases".
+	 * @param bases The base pairs to use
+	 * @param length The length of the base pair array.
+	 */
+	gnProteinSequence( gnSeqC *bases, const gnSeqI length);
+	/**
+	 * Copies the gnProteinSequence "seq".
+	 * @param seq The gnProteinSequence to copy.
+	 */
+	gnProteinSequence( const gnProteinSequence& seq);
 private:
-	boost::iostreams::mapped_file_source data;
-	std::string filename;
-}; // class gnRAWSequence
+	gnGenomeSpec *spec;
+	list<const gnBaseFilter*> filter_list;
+	const gnCompare* comparator;
+}; // class gnProteinSequence
 
-/*
-GNDLLEXPORT
-std::istream& operator>>(std::istream& is, gnRAWSequence& gns);	//read from source.
-GNDLLEXPORT
-std::ostream& operator<<(std::ostream& os, const gnRAWSequence& gns); //write to source.
-*/
-
+inline
+gnProteinSequence::gnProteinSequence() : gnSequence(){
+	filter_list.push_back(gnFilter::proteinSeqFilter());
+	comparator = gnCompare::ProteinSeqCompare();
+}
+inline
+gnProteinSequence::gnProteinSequence( const gnSeqC* seq ) : gnSequence(seq){
+	filter_list.push_back(gnFilter::proteinSeqFilter());
+	comparator = gnCompare::ProteinSeqCompare();
+}
+inline
+gnProteinSequence::gnProteinSequence( const std::string& str ) : gnSequence(str){
+	filter_list.push_back(gnFilter::proteinSeqFilter());
+	comparator = gnCompare::ProteinSeqCompare();
+}
+inline
+gnProteinSequence::gnProteinSequence( const gnGenomeSpec& gngs ) : gnSequence(gngs){
+	filter_list.push_back(gnFilter::proteinSeqFilter());
+	comparator = gnCompare::ProteinSeqCompare();
+}
+inline
+gnProteinSequence::gnProteinSequence( const gnFragmentSpec& gnfs ) : gnSequence(gnfs){
+	filter_list.push_back(gnFilter::proteinSeqFilter());
+	comparator = gnCompare::ProteinSeqCompare();
+}
+inline
+gnProteinSequence::gnProteinSequence( const gnContigSpec& gncs ) : gnSequence(gncs){
+	filter_list.push_back(gnFilter::proteinSeqFilter());
+	comparator = gnCompare::ProteinSeqCompare();
+}
+inline
+gnProteinSequence::gnProteinSequence( gnSeqC *bases, const gnSeqI length) : gnSequence(bases, length){
+	filter_list.push_back(gnFilter::proteinSeqFilter());
+	comparator = gnCompare::ProteinSeqCompare();
+}
+inline
+gnProteinSequence::gnProteinSequence( const gnProteinSequence& seq) : gnSequence(seq){
+	filter_list.push_back(gnFilter::proteinSeqFilter());
+	comparator = gnCompare::ProteinSeqCompare();
+}
 
 
 }	// end namespace genome
 
 #endif
-	// _gnRAWSequence_h_
+	// _gnProteinSequence_h_
