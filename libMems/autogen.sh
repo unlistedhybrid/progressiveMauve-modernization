ec3deac5229179a4da5849d1b6c59cde0ca73e07
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-17 19:31:36 -0500

    Delete libMems/autogen.sh

--- libMems/autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p config
-autoreconf --force --install -I config  -I m4
-echo "Now run ./configure --with-boost=</path/to/boost> --prefix=$HOME && make install"
-echo "Add --disable-shared to the configure line if building on Mac OS X"
