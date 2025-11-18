a46e930c10accf5a70f1d06f118f89e628605793
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-17 23:38:17 -0500

    Update algebras.cpp

--- libMems/libMems/HomologyHMM/algebras.cpp
+++ libMems/libMems/HomologyHMM/algebras.cpp
@@ -17,7 +17,7 @@
  *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 \*/
 //
-// algebras.cc - extended real types
+// algebras.cpp - extended real types
 //
 // Gerton Lunter, 27/8/04
 //
