0f4774a70ea13a8f4d5c7ce364aba57c989cfbf7
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-18 00:24:45 -0500

    Update util.c

--- libMems/libMems/dmSML/util.c
+++ libMems/libMems/dmSML/util.c
@@ -2,7 +2,7 @@
 #include "config.h"
 #endif
 
-#include <stdio.h>
+#include <cstdio>
 #include "libMems/dmSML/util.h"
 
 #define FMT_BUFFER_SIZE     (32)
@@ -25,7 +25,6 @@ const char * VFmt( const char * fmt, va_list args ) {
     if( ++FmtIdx >= FMT_BUFFER_SIZE ) {
         FmtIdx = 0;
     }
-    // silly windows....
 #ifdef WIN32
     _vsnprintf( FmtBuffer[FmtIdx], sizeof( FmtBuffer[FmtIdx] ), fmt, args );
 #else
@@ -36,8 +35,6 @@ const char * VFmt( const char * fmt, va_list args ) {
 }
 
 
-/// shifts a 64-bit value (in two 32 bit parts) either right or left.
-/// amt negative -> left, positive -> right
 void Shift64( int amt, int * hi, int * lo ) {
     if( amt == 0 ) {
         return;
@@ -60,7 +57,6 @@ void Shift64( int amt, int * hi, int * lo ) {
 void AddTo64( unsigned int amt, unsigned int *hi, unsigned int *lo ) {
 
     int i;
-    // holds each byte value.
     int in[8], out[8], tmp[8];
     int carry;
 
@@ -83,24 +79,6 @@ void AddTo64( unsigned int amt, unsigned int *hi, unsigned int *lo ) {
     tmp[7] = (*hi >> 24) & 0xFF;
     
 
-    /*
-    out[0] = (tmp[0] + in[0]);
-    carry = out[0] >> 8;
-    out[0] &= 0xFF;
-
-    out[1] = (tmp[1] + in[1] + carry);
-    carry = out[1] >> 8;
-    out[1] &= 0xFF;
-
-    out[2] = (tmp[2] + in[2] + carry);
-    carry = out[2] >> 8;
-    out[2] &= 0xFF;
-
-    out[3] = (tmp[3] + in[3] + carry);
-    carry = out[3] >> 8;
-    out[3] &= 0xFF;
-    */
-
     carry = 0;
     for( i = 0; i < 8; i++ ) {
         out[i] = in[i] + tmp[i] + carry;
@@ -108,20 +86,17 @@ void AddTo64( unsigned int amt, unsigned int *hi, unsigned int *lo ) {
         out[i] &= 0xFF;
     }
 
-    // convert back to 2-int form.
     *lo = out[0] + (out[1] << 8) + (out[2] << 16) + (out[3] << 24);
     *hi = out[4] + (out[5] << 8) + (out[6] << 16) + (out[7] << 24);
 
 }
 
-/** Utility function to delete a file */
 int removeFile( const char* filename, int verbose )
 {
 #ifdef WIN32
 		return remove( filename );
-//        return !DeleteFile( filename );
 #else
-        char* rm_cmd;
+        const char* rm_cmd;
         if( verbose )
                 rm_cmd = Fmt( "/bin/rm -fv %s", filename );
         else
@@ -129,4 +104,3 @@ int removeFile( const char* filename, int verbose )
         return system( rm_cmd );
 #endif
 }
-
