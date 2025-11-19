343e0219a9a635c691163a4644be8586c8b9589c
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-18 21:39:15 -0500

    Create getopt.h

---
+++ mauveAligner/src/getopt.h
@@ -0,0 +1,32 @@
+#ifndef _GETOPT_H_
+#define _GETOPT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern char *optarg;
+extern int optind;
+extern int opterr;
+extern int optopt;
+
+struct option {
+    const char *name;
+    int has_arg;
+    int *flag;
+    int val;
+};
+
+#define no_argument 0
+#define required_argument 1
+#define optional_argument 2
+
+int getopt(int argc, char *const argv[], const char *optstring);
+int getopt_long(int argc, char *const argv[], const char *optstring,
+                const struct option *longopts, int *longindex);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _GETOPT_H_
