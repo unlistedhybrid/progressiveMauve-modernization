b80a7140186e090322ca6db12821823319964752
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-20 03:34:42 -0500

    Update PairwiseMatchFinder.cpp

--- libMems/libMems/PairwiseMatchFinder.cpp
+++ libMems/libMems/PairwiseMatchFinder.cpp
@@ -36,37 +36,59 @@ PairwiseMatchFinder* PairwiseMatchFinder::Clone() const{
 // enumerate out every pairwise match
 bool PairwiseMatchFinder::EnumerateMatches( IdmerList& match_list ){
 
+	std::cerr << "DEBUG: PairwiseMatchFinder::EnumerateMatches called with " << match_list.size() << " items\n";
+	
+	if(match_list.empty()){
+		std::cerr << "DEBUG: match_list is empty, returning true\n";
+		return true;
+	}
+
 	match_list.sort(&idmer_id_lessthan);
+	std::cerr << "DEBUG: List sorted\n";
+	
 	IdmerList::iterator iter = match_list.begin();
-	IdmerList::iterator iter2 = match_list.begin();
-	uint cur_id_count = 1;
+	IdmerList::iterator iter_end = match_list.end();
+	unsigned int cur_id_count = 1;
 	IdmerList unique_list;
+	
 	// identify all of the unique seeds and add them to unique_list
-	while(iter2 != match_list.end()){
-		++iter2;
-		if(iter2 == match_list.end() || iter->id != iter2->id){
-			if( cur_id_count == 1 )
-				unique_list.push_back( *iter );
-			else
-				cur_id_count = 1;
-		}else
+	while(iter != iter_end){
+		IdmerList::iterator iter_next = iter;
+		++iter_next;
+		
+		if(iter_next == iter_end || iter->id != iter_next->id){
+			if(cur_id_count == 1){
+				std::cerr << "DEBUG: Adding unique seed with id " << iter->id << " to unique_list\n";
+				unique_list.push_back(*iter);
+			}
+			cur_id_count = 1;
+		}else{
 			cur_id_count++;
+		}
 		++iter;
 	}
+	
+	std::cerr << "DEBUG: Found " << unique_list.size() << " unique seeds\n";
+	
 	// hash each pair of unique seeds
-	boolean success = true;
-	for( iter = unique_list.begin(); iter != unique_list.end(); ++iter )
-	{
-		for( iter2 = iter; iter2 != unique_list.end(); ++iter2 )
-		{
-			if( iter == iter2 )
-				continue;
+	bool success = true;
+	IdmerList::iterator iter_a = unique_list.begin();
+	IdmerList::iterator iter_a_end = unique_list.end();
+	
+	for(; iter_a != iter_a_end; ++iter_a){
+		IdmerList::iterator iter_b = iter_a;
+		++iter_b;
+		
+		for(; iter_b != iter_a_end; ++iter_b){
+			std::cerr << "DEBUG: Hashing pair of seeds\n";
 			IdmerList hash_list;
-			hash_list.push_back( *iter );
-			hash_list.push_back( *iter2 );
+			hash_list.push_back(*iter_a);
+			hash_list.push_back(*iter_b);
 			success = success && HashMatch(hash_list);
 		}
 	}
+	
+	std::cerr << "DEBUG: PairwiseMatchFinder::EnumerateMatches returning " << (success ? "true" : "false") << "\n";
 	return success;
 }
 
