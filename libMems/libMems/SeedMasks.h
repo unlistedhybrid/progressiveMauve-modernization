821aed2fb01c38293901a112a3959c86a06498d8
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-19 21:53:00 -0500

    Update SeedMasks.h

--- libMems/libMems/SeedMasks.h
+++ libMems/libMems/SeedMasks.h
@@ -299,7 +299,6 @@ int64 getSeed( int weight, int seed_rank ){
 	uint32** masks;
 	int high;
 	int low;
-	int i = 1;
 	int64 seed = 0;
 	if( seed_rank == SOLID_SEED )
 		return getSolidSeed( weight );
@@ -335,8 +334,7 @@ inline static
 int getSeedLength( int64 seed ){
 	int right_bit = -1;
 	int left_bit = -1;
-	uint bitI = 0;
-	for( ; bitI < 64; ++bitI ){
+	for( uint bitI = 0; bitI < 64; ++bitI ){
 		if( (seed & 1) == 1 ){
 			left_bit = bitI;
 			if( right_bit == -1 )
@@ -362,8 +360,7 @@ inline static
 #endif
 int getSeedWeight( int64 seed ){
 	int weight = 0;
-	uint bitI = 0;
-	for( ; bitI < 64; ++bitI ){
+	for( uint bitI = 0; bitI < 64; ++bitI ){
 		if( (seed & 1) == 1 ){
 			++weight;
 		}
