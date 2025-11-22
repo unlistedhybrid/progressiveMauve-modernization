2ed1f178b6e319b34a6a19ffbb08e017b4bcdd7e
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-22 04:49:43 -0500

    Update MuscleInterface.cpp

--- libMems/libMems/MuscleInterface.cpp
+++ libMems/libMems/MuscleInterface.cpp
@@ -796,11 +796,12 @@ bool MuscleInterface::RefineFast( GappedAlignment& ga, size_t windowsize )
 	SetMaxIters(g_uMaxIters.get());
 	SetSeqWeightMethod(g_SeqWeight1.get());
 
-	MSA::SetIdCount(seq_table.size());
+	// Use aln_table (filtered) instead of seq_table (unfiltered)
+	MSA::SetIdCount(aln_table.size());
 
 	// create an MSA
 	MSA msa;
-	msaFromSeqTable(msa, seq_table);
+	msaFromSeqTable(msa, aln_table);
 
 	SetAlpha(ALPHA_DNA);
 	msa.FixAlpha();
