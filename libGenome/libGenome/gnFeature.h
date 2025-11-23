818f0d137b228eeaacdfcffdcb1ffa057f9483eb
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-23 05:30:02 -0500

    Update gnFeature.h

--- libGenome/libGenome/gnFeature.h
+++ libGenome/libGenome/gnFeature.h
@@ -2,20 +2,27 @@
 // File:            gnFeature.h
 // Purpose:         implements the gnBaseFeature for generic features
 // Description:     
+// Changes:        
+// Version:         libGenome 0.5.1 
 // Author:          Aaron Darling 
-// License:         See COPYING file for details 
+// Modified by:     
+// Copyright:       (c) Aaron Darling 
+// Licenses:        See COPYING file for details 
 /////////////////////////////////////////////////////////////////////////////
-#pragma once
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
+#ifndef _gnFeature_h_
+#define _gnFeature_h_
+
 #include "libGenome/gnDefs.h"
+
 #include <string>
 #include <vector>
 #include "libGenome/gnBaseFeature.h"
 #include "libGenome/gnBaseQualifier.h"
+
 
 namespace genome {
 
@@ -27,41 +34,41 @@ namespace genome {
 class GNDLLEXPORT gnFeature : public gnBaseFeature
 {
 public:
-    /**
-     * Empty constructor.
-     */
-    gnFeature();
+	/**
+	 * Empty constructor.
+	 */
+	gnFeature( );
+	/**
+	 * Creates a memory feature with the specified name.
+	 * @param name The name of the feature.
+	 * @param id The id number of the feature
+	 * @param lt The type of sequence location covered by this feature
+	 * @param broken True if the feature was broken by some sequence manipulation, false otherwise.
+	 */
+	gnFeature( std::string& name, uint32 id = 0, gnLocation::gnLocationType lt = gnLocation::LT_Nothing, bool broken = false );
+	/**
+	 * Copy constructor.
+	 * @param s The gnFeature to copy.
+	 */
+	gnFeature( const gnFeature& s );
+	/**
+	 * Destructor, frees memory.
+	 */
+	~gnFeature();
+// Clone
+	gnFeature* Clone() const;
 
-    /**
-     * Creates a memory feature with the specified name.
-     * @param name The name of the feature.
-     * @param id The id number of the feature.
-     * @param lt The type of sequence location covered by this feature.
-     * @param broken True if the feature was broken by some sequence manipulation, false otherwise.
-     */
-    gnFeature(const std::string& name, uint32 id = 0,
-              gnLocation::gnLocationType lt = gnLocation::LT_Nothing,
-              bool broken = false);
+private:
+}; // class gnFeature
 
-    /**
-     * Copy constructor.
-     * @param s The gnFeature to copy.
-     */
-    gnFeature(const gnFeature& s);
-
-    /**
-     * Destructor, frees memory.
-     */
-    ~gnFeature();
-
-    // Clone
-    gnFeature* Clone() const;
-};
-
-// Inline Clone implementation
-inline gnFeature* gnFeature::Clone() const
+inline
+gnFeature* gnFeature::Clone() const
 {
-    return new gnFeature(*this);
+	return new gnFeature(*this);
 }
 
-} // end namespace genome
+
+}	// end namespace genome
+
+#endif
+	// _gnFeature_h_
