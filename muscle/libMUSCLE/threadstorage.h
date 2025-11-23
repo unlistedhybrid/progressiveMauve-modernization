1cd9a184a099aa52e847c56fcab040c5475b2816
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-23 02:06:02 -0500

    Update threadstorage.h

--- muscle/libMUSCLE/threadstorage.h
+++ muscle/libMUSCLE/threadstorage.h
@@ -1,8 +1,6 @@
 #ifndef _threadstorage_h_
 #define _threadstorage_h_
 
-// Portable thread-local storage for MUSCLE, supporting MSVC, GCC, Clang, OpenMP, single-threaded
-
 #if defined(_MSC_VER)
     #define TLS_DECL __declspec(thread)
 #elif defined(__GNUC__) || defined(__clang__)
@@ -11,30 +9,45 @@
     #define TLS_DECL
 #endif
 
+// Default TLS: for pointer types
 template <typename T>
 class TLS
 {
 public:
     TLS() {}
-    explicit TLS(T t_val) { value = t_val; }
-
-    // Use .get() everywhere for cross-platform compatibility
-    T* get() const { return value; }
     void set(T* v) { value = v; }
-
-    // Operator overloads for code using g_SeqVec->method()
+    T* get() const { return value; }
     T* operator->() const { return value; }
     T& operator*()  const { return *value; }
 
 private:
-    // No copying
     TLS(const TLS&) = delete;
     TLS& operator=(const TLS&) = delete;
-
     TLS_DECL static T* value;
 };
 
 template <typename T>
 TLS_DECL T* TLS<T>::value = nullptr;
+
+// Partial specialization for arrays
+template <typename T, size_t N>
+class TLS<T[N]>
+{
+public:
+    TLS() {}
+    T* get() { return value; }
+    const T* get() const { return value; }
+    T& operator[](size_t idx) { return value[idx]; }
+    const T& operator[](size_t idx) const { return value[idx]; }
+    // Assignment from array or pointer
+    void set(const T* v) { for (size_t i = 0; i < N; ++i) value[i] = v[i]; }
+private:
+    TLS(const TLS&) = delete;
+    TLS& operator=(const TLS&) = delete;
+    TLS_DECL static T value[N];
+};
+
+template <typename T, size_t N>
+TLS_DECL T TLS<T[N]>::value[N] = {};
 
 #endif // _threadstorage_h_
