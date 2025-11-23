e0f085e5c1d018e8fa8a51399c56902d770b0a4a
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-23 05:30:32 -0500

    Update gnFileSource.h

--- libGenome/libGenome/gnFileSource.h
+++ libGenome/libGenome/gnFileSource.h
@@ -1,9 +1,11 @@
 /////////////////////////////////////////////////////////////////////////////
 // File:            gnFileSource.h
 // Purpose:         Implements gnBaseSource for .File files
-// Description:     Standard interface for file-based genetic sources
+// Description:     
+// Changes:        
 // Version:         libGenome 0.5.1 
 // Author:          Aaron Darling 
+// Modified by:     
 // Copyright:       (c) Aaron Darling 
 // Licenses:        See COPYING file for details 
 /////////////////////////////////////////////////////////////////////////////
@@ -15,6 +17,7 @@
 #define _gnFileSource_h_
 
 #include "libGenome/gnDefs.h"
+
 #include <string>
 #include <fstream>
 #include "libGenome/gnBaseSource.h"
@@ -24,62 +27,67 @@
 namespace genome {
 
 /**
- * Interface for all file-based sources of genetic information.
+ * gnFileSource is a standard interface to all file based sources of genetic information.
+ * All file source classes are derived from this class.
  */
-class GNDLLEXPORT gnFileSource : public gnBaseSource {
+class GNDLLEXPORT gnFileSource : public gnBaseSource
+{
 public:
-    gnFileSource();
-    gnFileSource(const gnFileSource& gnfs);
-    virtual ~gnFileSource() = default;
+	gnFileSource();
+	gnFileSource(const gnFileSource& gnfs);
+	virtual ~gnFileSource(){}
+	virtual gnFileSource* Clone() const = 0;
+	// Open, Close	
+	virtual void Open( std::string openString );
+	virtual void Open();
+	virtual void Close();
+	virtual std::string GetOpenString() const ;
+	  // Filter
+	virtual const gnFilter* GetFilter() const ;
+	virtual void SetFilter( gnFilter* filter );
 
-    [[nodiscard]] virtual gnFileSource* Clone() const = 0;
-
-    // Open, Close
-    virtual void Open(const std::string& openString);
-    virtual void Open();
-    virtual void Close();
-
-    [[nodiscard]] virtual std::string GetOpenString() const noexcept;
-
-    // Filter
-    [[nodiscard]] virtual const gnFilter* GetFilter() const noexcept;
-    virtual void SetFilter(gnFilter* filter);
-
-    virtual bool Read(uint64 pos, char* buf, gnSeqI& bufLen);
-
-    /**
-     * Returns a pointer to the file contig for contigI, or nullptr if not found.
-     */
-    virtual gnFileContig* GetFileContig(uint32 contigI) const = 0;
-
+	virtual bool Read( const uint64 pos, char* buf, gnSeqI& bufLen );
+	/**
+	 * Returns a pointer to the file contig corresponding to contigI or
+	 * null if none exists.
+	 */
+	virtual gnFileContig* GetFileContig( const uint32 contigI ) const = 0;
 protected:
-    void DetermineNewlineType();
+	void DetermineNewlineType();
 
-    std::string m_openString;
-    std::ifstream m_ifstream;
-    const gnFilter* m_pFilter{nullptr};
-    gnNewlineType m_newlineType{GN_NEWLINE_UNKNOWN};
-    uint32 m_newlineSize{0};
+	std::string m_openString;
+	std::ifstream m_ifstream;
+	const gnFilter* m_pFilter;
+	gnNewlineType m_newlineType;
+	uint32 m_newlineSize;
 
 private:
-    virtual bool ParseStream(std::istream& fin) = 0;
-}; // class gnFileSource
+	virtual bool ParseStream( std::istream& fin ) = 0;
+};// class gnFileSource
 
-// ---- Inline implementations ----
-
-inline std::string gnFileSource::GetOpenString() const noexcept {
-    return m_openString;
+inline
+std::string gnFileSource::GetOpenString( ) const
+{
+	return m_openString;
 }
-inline const gnFilter* gnFileSource::GetFilter() const noexcept {
-    return m_pFilter;
-}
-inline void gnFileSource::SetFilter(gnFilter* filter) {
-    if (filter == nullptr) {
-        Throw_gnEx(NullPointer());
-    }
-    m_pFilter = filter;
+// Filter
+inline
+const gnFilter* gnFileSource::GetFilter() const
+{
+	return m_pFilter;
 }
 
-} // end namespace genome
+inline
+void gnFileSource::SetFilter( gnFilter* filter )
+{
+	if(filter == nullptr){
+		Throw_gnEx(NullPointer());
+	}
+	m_pFilter = filter;
+}
 
-#endif // _gnFileSource_h_
+
+}	// end namespace genome
+
+#endif
+	// _gnFileSource_h_
