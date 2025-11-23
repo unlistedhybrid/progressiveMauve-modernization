6f8f575d9b0bde775019252efc7a3a3b89d2c2b3
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-23 05:30:16 -0500

    Update gnFeature.cpp

--- libGenome/libGenome/gnFeature.cpp
+++ libGenome/libGenome/gnFeature.cpp
@@ -1,9 +1,13 @@
-// gnFeature.cpp
 /////////////////////////////////////////////////////////////////////////////
 // File:            gnFeature.cpp
 // Purpose:         implements the gnBaseFeature for generic features
+// Description:     
+// Changes:        
+// Version:         libGenome 0.5.1 
 // Author:          Aaron Darling 
-// License:         See COPYING file for details
+// Modified by:     
+// Copyright:       (c) Aaron Darling 
+// Licenses:        See COPYING file for details
 /////////////////////////////////////////////////////////////////////////////
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -15,41 +19,37 @@
 #include "libGenome/gnDebug.h"
 #include <list>
 
+
+using namespace std;
 namespace genome {
 
+
 gnFeature::gnFeature()
-    : gnBaseFeature()  // If gnBaseFeature has a default constructor
 {
-    // m_id = 0;
-    // m_name = "";
-    // m_locationType = gnLocation::LT_Nothing;
-    // m_broken = false;
-}
-
-gnFeature::gnFeature(const std::string& name, uint32 id,
-                     gnLocation::gnLocationType lt, bool broken)
-    : gnBaseFeature(name, id, nullptr, lt, broken)
+/*	m_id = 0;
+	m_name = "";
+	m_locationType = gnLocation::LT_Nothing;
+	m_broken = false;
+*/}
+gnFeature::gnFeature( string& name, uint32 id, gnLocation::gnLocationType lt, bool broken )
+ : gnBaseFeature(name, id, nullptr, lt, broken)
 {
 }
-
-gnFeature::gnFeature(const gnFeature& s)
-    : gnBaseFeature(s)
+gnFeature::gnFeature( const gnFeature& s )
 {
-    m_id = s.m_id;
-    m_name = s.m_name;
-    m_locationType = s.m_locationType;
-    m_broken = s.m_broken;
-    m_spec = s.m_spec;
-    for (const auto& loc : s.m_locationList)
-        m_locationList.push_back(loc);
-    for (const auto& qual : s.m_qualifierList)
-        m_qualifierList.push_back(qual->Clone());
+	uint32 i;
+	m_id = s.m_id;
+	m_name = s.m_name;
+	m_locationType = s.m_locationType;
+	m_broken = s.m_broken;
+	m_spec = s.m_spec;
+	for( i=0; i < s.m_locationList.size(); i++)
+		m_locationList.push_back(s.m_locationList[i]);
+	for( i=0; i < s.m_qualifierList.size(); i++)
+		m_qualifierList.push_back(s.m_qualifierList[i]->Clone());
 }
-
 gnFeature::~gnFeature()
 {
-    // If m_qualifierList stores owning pointers, consider deleting them here.
-    // If using smart pointers, this is not needed.
 }
 
-} // end namespace genome
+}	// end namespace genome
