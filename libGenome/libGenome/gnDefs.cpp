02b6d1c642d0d343994b4eb24a0896d095585c20
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-23 04:37:57 -0500

    Update gnDefs.cpp

--- libGenome/libGenome/gnDefs.cpp
+++ libGenome/libGenome/gnDefs.cpp
@@ -4,8 +4,11 @@
 
 #include "libGenome/gnDefs.h"
 
+namespace genome {
+
 // some compilers don't have abs() for 64 bit ints
-int64 abs_int64(int64 a){
+int64 abs_int64(int64 a) {
 	return a < 0 ? -a : a;
 }
 
+} // namespace genome
