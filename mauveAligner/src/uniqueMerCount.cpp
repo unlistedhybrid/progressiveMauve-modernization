c68a0960c06ae101c92b0624ed02a9958384f000
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-23 04:12:27 -0500

    Update uniqueMerCount.cpp

--- mauveAligner/src/uniqueMerCount.cpp
+++ mauveAligner/src/uniqueMerCount.cpp
@@ -1,9 +1,7 @@
 /*******************************************************************************
  * $Id: uniqueMerCount.cpp,v 1.1 2004/02/28 00:01:31 darling Exp $
- * This file is copyright 2002-2004 Aaron Darling.  All rights reserved.
- * Please see the file called COPYING for licensing, copying, and modification
- * rights.  Redistribution of this file, in whole or in part is prohibited
- * without express permission.
+ * Copyright 2002-2004 Aaron Darling. All rights reserved.
+ * Please see the file COPYING for licensing and rights.
  ******************************************************************************/
 
 #ifdef HAVE_CONFIG_H
@@ -11,31 +9,33 @@
 #endif
 
 #include "libMems/DNAFileSML.h"
+#include <iostream>
+#include <memory>
+#include <string>
 
-using namespace std;
-using namespace genome;
-using namespace mems;
-
-void print_usage( const char* pname ){
-	cerr << "Usage: " << pname << " <Sorted Mer List>\n";
+namespace {
+    void print_usage(const char* pname) {
+        std::cerr << "Usage: " << pname << " <Sorted Mer List>\n";
+    }
 }
 
-int main( int argc, const char* argv[] ){
-	if( argc != 2 ){
-		print_usage("uniqueMerCount");
-		return -1;
-	}
+int main(int argc, const char* argv[]) {
+    if (argc != 2) {
+        print_usage(argv[0]);
+        return -1;
+    }
 
-	string sml_filename = argv[1];
-	DNAFileSML* file_sml = new DNAFileSML();
-	boolean success = true;
-	try{
-		file_sml->LoadFile( sml_filename );
-	}catch( gnException& gne ){
-		success = false;
-		cerr << gne << endl;
-		return -1;
-	}
-	cout << endl << file_sml->UniqueMerCount() << endl;
+    std::string sml_filename = argv[1];
+    std::unique_ptr<genome::mems::DNAFileSML> file_sml = std::make_unique<genome::mems::DNAFileSML>();
+
+    bool success = true;
+    try {
+        file_sml->LoadFile(sml_filename);
+    } catch (const genome::gnException& gne) {
+        success = false;
+        std::cerr << gne << std::endl;
+        return -1;
+    }
+    std::cout << std::endl << file_sml->UniqueMerCount() << std::endl;
+    return 0;
 }
-
