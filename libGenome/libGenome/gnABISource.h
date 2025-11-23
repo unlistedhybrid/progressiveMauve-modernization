27ad86d2f783d7a5be4f12817d6b770efe8b292f
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-23 05:11:48 -0500

    Update gnABISource.h

--- libGenome/libGenome/gnABISource.h
+++ libGenome/libGenome/gnABISource.h
@@ -1,53 +1,94 @@
-#ifndef GN_ABI_SOURCE_H
-#define GN_ABI_SOURCE_H
+/////////////////////////////////////////////////////////////////////////////
+// File:            gnABISource.h
+// Purpose:         Implements gnBaseSource for ABI files
+// Description:     
+// Changes:        
+// Version:         libGenome 0.5.1 
+// Author:          Aaron Darling 
+// Modified by:     
+// Copyright:       (c) Aaron Darling 
+// Licenses:        See COPYING file for details 
+/////////////////////////////////////////////////////////////////////////////
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
-#include <string>
-#include <vector>
-#include <memory>
-#include <iosfwd>
+#ifndef _gnABISource_h_
+#define _gnABISource_h_
 
 #include "libGenome/gnDefs.h"
+#include <string>
+#include "libGenome/gnSequence.h"
 #include "libGenome/gnFileSource.h"
 #include "libGenome/gnFileContig.h"
+#include "libGenome/gnSourceSpec.h"
 #include "libGenome/gnGenomeSpec.h"
 #include "libGenome/gnFilter.h"
 
 namespace genome {
 
-class gnABISource : public gnFileSource {
+/**
+ * gnABISource is not implemented.
+ */
+class GNDLLEXPORT gnABISource : public gnFileSource
+{
 public:
-    gnABISource();
-    gnABISource(const gnABISource& s);
-    ~gnABISource();
+	gnABISource();	
+	/**
+	 * Clone Constructor copies the specified gnSEQSource.
+	 * @param s The gnABISource to copy.
+	 */
+	gnABISource( const gnABISource& s );
+	~gnABISource();
+	gnABISource* Clone() const;
 
-    // Contig access methods
-    bool HasContig(const std::string& name) const;
-    uint32 GetContigID(const std::string& name) const;
-    std::string GetContigName(uint32 i) const;
-    gnSeqI GetContigSeqLength(uint32 i) const;
-    
-    gnFileContig* GetFileContig(const uint32 contigI) const;
-    gnGenomeSpec* GetSpec() const;
+	uint32 GetContigListLength() const;
+	bool HasContig( const std::string& name ) const;
+	uint32 GetContigID( const std::string& name ) const;
+	std::string GetContigName( const uint32 i ) const;
+	gnSeqI GetContigSeqLength( const uint32 i ) const;
+	gnFileContig* GetContig( const uint32 i ) const;
 
-    // Sequence reading
-    bool SeqRead(const gnSeqI start, char* buf, gnSeqI& bufLen, const uint32 contigI);
-    bool SeqSeek(const gnSeqI start, const uint32& contigI, uint64& startPos, uint64& readableBytes);
-    bool SeqStartPos(const gnSeqI start, gnFileContig& contig, uint64& startPos, uint64& readableBytes);
+	bool SeqRead( const gnSeqI start, char* buf, gnSeqI& bufLen, const uint32 contigI=ALL_CONTIGS );
 
-    // Stream parsing
-    bool ParseStream(std::istream& fin);
+	/**
+	 * Writes the specified gnSequence to an ABI file named "filename".
+	 * @param sequence The gnSequence to write out.
+	 * @param filename The name of the file to write.
+	 * @return True if successful, false otherwise.
+	 */
+	static bool Write(gnSequence& sequence, const std::string& filename);
+	gnGenomeSpec *GetSpec() const;
 
-protected:
-    std::vector<gnFileContig*> m_contigList;
-    gnGenomeSpec* m_spec;
-    std::string m_openString;
-    std::shared_ptr<gnFilter> m_pFilter;
-
+	gnFileContig* GetFileContig( const uint32 contigI ) const;
 private:
-    // Not implemented
-    bool Write(gnSequence& seq, const std::string& filename);
-};
+	bool SeqSeek( const gnSeqI start, const uint32& contigI, uint64& startPos, uint64& readableBytes );
+	bool SeqStartPos( const gnSeqI start, gnFileContig& contig, uint64& startPos, uint64& readableBytes );
+	bool ParseStream( std::istream& fin );
 
-} // namespace genome
+	gnGenomeSpec *m_spec;
+	std::vector< gnFileContig* > m_contigList;	
 
-#endif // GN_ABI_SOURCE_H
+};// class gnABISource
+
+inline
+gnABISource* gnABISource::Clone() const
+{
+	return new gnABISource( *this );
+}
+inline
+uint32 gnABISource::GetContigListLength() const
+{
+	return m_contigList.size();
+}
+inline
+gnGenomeSpec *gnABISource::GetSpec() const
+{
+	return m_spec->Clone();
+}
+
+
+}	// end namespace genome
+
+#endif
+	// _gnABISource_h_
