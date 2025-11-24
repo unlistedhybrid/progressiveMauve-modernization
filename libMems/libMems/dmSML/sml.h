f703bb3720b9c58a4b265c6b6f021683e5c87029
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-24 01:53:43 -0500

    Update sml.h

--- libMems/libMems/dmSML/sml.h
+++ libMems/libMems/dmSML/sml.h
@@ -7,13 +7,13 @@
 
 #include "libMems/dmSML/asyncio.h"
 #include <cstring>
-#include <cstdint>
-#include <memory>
-#include <string>
-#include "libGenome/gnDefs.h"
+#include <cstdint> // For standard integer types
+#include <memory>  // For std::unique_ptr
+#include "libGenome/gnDefs.h" // For boolean, etc.
 
 namespace sml {
 
+// --- Type aliases using standard C++ types ---
 using uint8     = std::uint8_t;
 using uint32    = std::uint32_t;
 using uint64    = std::uint64_t;
@@ -21,16 +21,22 @@ using position_t = uint32;
 using mask_t    = uint64;
 using sarID_t   = std::int16_t;
 
+// --- Constants (Renamed to avoid macro collisions) ---
+// UINT8_MAX
 constexpr std::size_t UINT8_MAX_VALUE = 256;
+// DESCRIPTION_SIZE
 constexpr std::size_t SML_DESCRIPTION_SIZE = 2048; 
+// DNA_ALPHA_BITS value (used in MemorySML.h)
 constexpr std::uint32_t SML_DNA_ALPHA_BITS = 2;
-constexpr mask_t SEED_MASK = 0x7FFFFFFF;
-constexpr int MASK_T_BYTES = 8;
 
-inline mask_t seed_mask = SEED_MASK;
+// --- Masking parameters (Made inline for header definition) ---
+constexpr int MASK_T_BYTES = 8;
+inline mask_t seed_mask = 0x7FFFFFFF;
 inline int mask_length = 31;
 inline int mask_weight = 31;
 
+// --- Create the basic DNA translation table ---
+// Returns a raw pointer whose ownership is released to the caller (dmsort.cpp)
 inline uint8* CreateBasicDNATable() {
     auto bdt = std::make_unique<uint8[]>(UINT8_MAX_VALUE);
     std::memset(bdt.get(), 0, UINT8_MAX_VALUE);
@@ -41,29 +47,33 @@ inline uint8* CreateBasicDNATable() {
     return bdt.release();
 }
 
-struct SMLHeader_t {
-    uint32 version;
-    uint32 alphabet_bits;
-    uint64 seed;
-    uint32 seed_length;
-    uint32 seed_weight;
-    uint64 length;
-    uint32 unique_mers;
-    uint32 word_size;
-    bool little_endian;
-    sarID_t id;
-    bool circular;
-    uint8 translation_table[UINT8_MAX_VALUE];
-    char description[SML_DESCRIPTION_SIZE];
-};
+// FIX: DNA_TABLE is no longer static in the header; dmsort.cpp defines it globally.
+// static uint8* DNA_TABLE; 
 
-struct sml_t {
-    char key[8];
-    position_t pos;
-};
+typedef struct SMLHeader_s{
+	uint32 version;
+	uint32 alphabet_bits;
+	uint64 seed;
+	uint32 seed_length;
+	uint32 seed_weight;
+	uint64 length;
+	uint32 unique_mers;
+	uint32 word_size;
+	boolean little_endian;
+	signed short id;
+	boolean circular;
+	uint8 translation_table[UINT8_MAX_VALUE]; // Use C++ constant
+	char description[SML_DESCRIPTION_SIZE];   // Use renamed constant
+} SMLHeader_t;
 
-SMLHeader_t InitSML(aFILE* file, uint64 file_size, uint64 seed);
 
-}
+typedef struct sml_s {
+	char key[8];
+	position_t pos;
+} sml_t;
 
-#endif
+SMLHeader_t InitSML( aFILE* file, uint64 file_size, uint64 seed );
+
+} // namespace sml
+
+#endif /* _sml_h_ */
