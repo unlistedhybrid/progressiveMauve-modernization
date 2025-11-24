b48e3d9b27c3c080682279281bfea94b8aead7a4
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-24 01:39:19 -0500

    Update DNAMemorySML.h

--- libMems/libMems/DNAMemorySML.h
+++ libMems/libMems/DNAMemorySML.h
@@ -1,11 +1,3 @@
-/*******************************************************************************
- * $Id: DNAMemorySML.h,v 1.3 2004/03/01 02:40:08 darling Exp $
- * This file is copyright 2002-2007 Aaron Darling and authors listed in the AUTHORS file.
- * This file is licensed under the GPL.
- * Please see the file called COPYING for licensing details.
- * **************
- ******************************************************************************/
-
 #ifndef _DNAMemorySML_h_
 #define _DNAMemorySML_h_
 
@@ -14,21 +6,22 @@
 #endif
 
 #include "libGenome/gnSequence.h"
-#include "libMems/SortedMerList.h"
+#include "libMems/SortedMerList.h" 
+#include "libMems/MemorySML.h"
 #include "libMems/dmSML/sml.h"
 #include <vector>
+#include <cstdint>
 
 namespace mems {
 
 class DNAMemorySML : public mems::MemorySML
 {
 public:
-	DNAMemorySML(const std::uint8_t* table = SortedMerList::BasicDNATable(), const std::uint32_t alpha_bits = sml::DNA_ALPHA_BITS);
+	DNAMemorySML(const std::uint8_t* table = SortedMerList::BasicDNATable(), const std::uint32_t alpha_bits = sml::SML_DNA_ALPHA_BITS);
 	DNAMemorySML(const DNAMemorySML& msa);
 	DNAMemorySML(const SortedMerList& sa);
 	DNAMemorySML& operator=(const DNAMemorySML& msa );
 	DNAMemorySML* Clone() const;
-	
 	
 	virtual std::uint64_t GetMer(gnSeqI offset) const;
 	virtual std::uint64_t GetSeedMer( gnSeqI offset ) const;
@@ -42,4 +35,3 @@ protected:
 }
 
 #endif
-
