ff5ea76eff0ef87e7d0ff77c4e0f60153da77d09
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-24 01:23:32 -0500

    Update DNAMemorySML.h

--- libMems/libMems/DNAMemorySML.h
+++ libMems/libMems/DNAMemorySML.h
@@ -15,34 +15,23 @@
 
 #include "libGenome/gnSequence.h"
 #include "libMems/MemorySML.h"
+#include "libMems/dmSML/sml.h"
+#include <vector>
 
 namespace mems {
 
-/** The DNAMemorySML is an implementation of sorted mer lists which creates and
- *  stores the sorted mer list entirely in memory.  A DNAMemorySML consumes
- *  roughly 32 + alpha_bits bits of memory per character in the sequences.
- *  For unambiguous DNA sequences 4.25 bytes per base are required.
- *  The seed pattern for DNA SMLs must be palindromic
- */
-class DNAMemorySML : public MemorySML
+class DNAMemorySML : public mems::MemorySML
 {
 public:
-	/** 
-	 *  Create an empty DNAMemorySML
-	 *  Creates an empty DNAMemorySML with the supplied translation
-	 *  table and alphabet bit size.  Defaults to DNA settings
-	 *  @param table The array used to translate characters into binary code
-	 *  @param alpha_bits The number of bits each character consumes in binary
-	 */
-	DNAMemorySML(const uint8* table = SortedMerList::BasicDNATable(), const uint32 alpha_bits = DNA_ALPHA_BITS);
+	DNAMemorySML(const std::uint8_t* table = SortedMerList::BasicDNATable(), const std::uint32_t alpha_bits = sml::DNA_ALPHA_BITS);
 	DNAMemorySML(const DNAMemorySML& msa);
 	DNAMemorySML(const SortedMerList& sa);
 	DNAMemorySML& operator=(const DNAMemorySML& msa );
 	DNAMemorySML* Clone() const;
 	
 	
-	virtual uint64 GetMer(gnSeqI offset) const;
-	virtual uint64 GetSeedMer( gnSeqI offset ) const;
+	virtual std::uint64_t GetMer(gnSeqI offset) const;
+	virtual std::uint64_t GetSeedMer( gnSeqI offset ) const;
 	
 protected:
 
@@ -52,4 +41,4 @@ protected:
 
 }
 
-#endif   //_DNAMemorySML_h_
+#endif
