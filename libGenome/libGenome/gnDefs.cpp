d1b32b7a8fde3382b7f8e6344b14d5f32ce9eda5
unlistedhybrid <aaronstarseed@gmail.com>
2025-11-23 22:42:11 -0500

    Update gnDefs.cpp

--- libGenome/libGenome/gnDefs.cpp
+++ libGenome/libGenome/gnDefs.cpp
@@ -4,9 +4,9 @@
 
 #include "libGenome/gnDefs.h"
 
-#ifndef HAVE_LLABS
-// some compilers don't have abs() for 64 bit ints, so we define it here
-int64 abs( int64 a ) noexcept {
-	return a < 0 ? -a : a;
+namespace libGenome { // <--- Put the definition inside the namespace
+    // some compilers don't have abs() for 64 bit ints
+    int64 abs( int64 a ) noexcept {
+        return a < 0 ? -a : a;
+    }
 }
-#endif
